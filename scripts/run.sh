ad02a81169e9a713740ecf49631b11564830b796
lushun <lushun@kuaishou.com>
2021-02-27 14:29:15 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+export DATA_ROOT=/home/lushun/dataset
+export METIS_WORKER_0_HOST=23456
+export METIS_WORKER_0_PORT=22
+export METIS_TASK_INDEX=0
+export ARNOLD_WORKER_NUM=16
+export ARNOLD_WORKER_GPU=8
+export ARNOLD_OUTPUT=/home/yanglongxing/project/yet_another_mobilenet_series/output
+
 source ./scripts/utils/setup.sh
 source ./scripts/utils/export_data.sh
 bash ./scripts/utils/remote_run.sh $@
