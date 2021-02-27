76fc5281cb1759933ebb628b95dbb19e0a341921
lushun <lushun@kuaishou.com>
2021-02-27 14:32:13 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ export METIS_WORKER_0_PORT=22
 export METIS_TASK_INDEX=0
 export ARNOLD_WORKER_NUM=16
 export ARNOLD_WORKER_GPU=8
-export ARNOLD_OUTPUT=/home/yanglongxing/project/yet_another_mobilenet_series/output
+export ARNOLD_OUTPUT=/home/lushun/code/yet_another_mobilenet_series/output
 
 source ./scripts/utils/setup.sh
 source ./scripts/utils/export_data.sh
