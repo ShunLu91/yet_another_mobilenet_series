88cdfa477cd23286874da20c37e1336af8aa849c
lushun <lushun@kuaishou.com>
2021-02-27 14:34:03 +0800

    update

--- scripts/utils/export_data.sh
+++ scripts/utils/export_data.sh
@@ -5,7 +5,7 @@ export DATA_LMDB=~/data/imagenet_lmdb
 
 if [ ! -d "${DATA}" ]; then
     echo 'use remote imagenet'
-    export DATA=${DATA_ROOT}/imagenet
+    export DATA=${DATA_ROOT}/ImageNet
 fi
 if [ ! -d "${DATA_LMDB}" ]; then
     echo 'use remote imagenet_lmdb'
